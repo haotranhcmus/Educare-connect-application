33618af79637c576f2b3ec1ca8c5c6771ace68e7
Hao Nhut Tran <haotranhcmus@gmail.com>
2026-03-16 16:56:14 +0700

    Revert "Feature/educare iep"

--- addons/educare_iep/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import goal_quick_wizard
-from . import objective_quick_wizard
-from . import plan_close_wizard
-from . import plan_revision_wizard
-from . import objective_discontinue_wizard
--- addons/educare_student/models/__init__.py
+++ addons/educare_student/models/__init__.py
@@ -1,4 +1,2 @@
-from . import user_profile_ext
-from . import educare_transfer
-from . import educare_assessment
 from . import educare_student
+from . import user_profile_ext
